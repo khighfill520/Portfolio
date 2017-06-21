1fbdd3fd0cccf1363f59b39cf298442bdcd0526e
Kimber Highfill <khighfill520@gmail.com>
2017-06-21 09:27:27 -0400

    Update vdice.js

--- vdice.js
+++ vdice.js
@@ -19,9 +19,45 @@ function baseroll() {
  
   
   //get and display sum
-  for (k = 0; k < firstnum; k++) {
-    sum = (sum + parseInt(rolls[k]));
+  for (j = 0; j < firstnum; j++) {
+    sum = (sum + parseInt(rolls[j]));
   }
   sum = (sum + thirdnum);
+  document.getElementById("sum").innerHTML = sum;
+}
+
+function advroll() {
+  
+  //set variables
+  var firstnum = parseInt(document.getElementById("fnum1").value);
+  var secnum = parseInt(document.getElementById("snum1").value);
+  var thirdnum = parseInt(document.getElementById("tnum1").value);
+  var fourthnum = parseInt(document.getElementById("fonum").value);
+  var fifthnum = parseInt(document.getElementById("finum").value);
+  var i;
+  var j;
+  var k;
+  var sum = 0;
+  var rolls = [];
+  var rolls2;
+  
+  //get first rolls 
+  for (i = 0; i < firstnum; i++) {
+      rolls[i] = Math.floor(Math.random()*secnum+1);
+  }
+  
+  //get second rolls and display
+  for (j = (i + 1); j < (firstnum + thirdnum - 1); j++) {
+    rolls[j] = Math.floor(Math.random()*fourthnum+1);
+  }
+  rolls2 = rolls.toString();
+  document.getElementById("values").innerHTML = rolls2;
+ 
+  
+  //get and display sum
+  for (k = 0; k <= j; k++) {
+    sum = (sum + parseInt(rolls[k]));
+  }
+  sum = (sum + fifthnum);
   document.getElementById("sum").innerHTML = sum;
 }
