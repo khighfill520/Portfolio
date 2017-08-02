48cbb2a1745c943700db799dd0d2511e7c2cdab8
Kimber Highfill <khighfill520@gmail.com>
2017-08-01 20:44:14 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -5,7 +5,7 @@ function sudoku(value) {
     div = document.getElementById('puzzle');
     table = document.createElement('table');
     text = document.createElement('input');
-    buttons = document.getElementsByTagName('input');
+    inputs = document.getElementsByTagName('input');
     var h;
     var i;
     var j;
@@ -13,12 +13,6 @@ function sudoku(value) {
     var tr;
     var td;
     
-    //remove buttons and generate table
-    for(var h = 0; h < buttons.length; i++) {
-        if(buttons[h].type.toLowerCase() == 'submit') {
-            buttons[h].parentNode.removeChild(buttons[h]); 
-        }
-    }   
     for (i = 1; i < 10; i++) {
         tr = table.insertRow();
         if ((i % 3) == 0) {
@@ -30,6 +24,13 @@ function sudoku(value) {
             if ((j % 3) == 0) {
                 td.setAttribute('class', 'heavy_right');
             }
+        }
+    }
+    for(var h = 0; h < inputs.length; i++) {
+        if(inputs[h].type.toLowerCase() == 'submit') {
+            inputs[h].parentNode.removeChild(inputs[h]); 
+        } else {
+            inputs[h].type = "text";
         }
     }   
     div.appendChild(table);
