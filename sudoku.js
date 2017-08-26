80d656ff817a6c34b5043d6ac29e1beb5ff4dba0
Kimber Highfill <khighfill520@gmail.com>
2017-08-25 23:46:12 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -5,11 +5,10 @@ function sudoku(value) {
     var div = document.getElementById('puzzle');
     var table = document.createElement('table');
     var inputs = document.getElementsByTagName('input');
-    var letter = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
     var easy = [1, 2, 3, 5, 7, 8, 9, 10, 12, 14, 16, 18, 19, 20, 21, 24, 25, 26, 27, 30, 31, 32, 33, 37, 38, 40, 41, 42, 44, 45, 49, 50, 51, 52, 55, 56, 57, 58, 61, 62, 63, 64, 66, 68, 70, 72, 73, 74, 75, 77, 79, 80, 81];
     var med = [4, 5, 6, 7, 8, 10, 13, 15, 19, 23, 24, 28, 29, 30, 32, 35, 36, 37, 39, 40, 41, 42, 43, 45, 46, 47, 50, 52, 53, 54, 58, 59, 63, 67, 69, 72, 74, 75, 76, 77, 78];
     var hard = [2, 6, 11, 12, 15, 17, 18, 21, 24, 25, 26, 28, 29, 30, 52, 53, 54, 56, 57, 58, 61, 64, 65, 67, 70, 71, 76, 80];
-    var heavy == "";
+    var heavy = "";
     var g;
     var h;
     var nums = [];
@@ -82,29 +81,90 @@ function sudoku(value) {
         }
     }
     
-    //generate array of numbers
-    for (n = 0; n < 82; n++) {
-        nums[n] = Math.floor(Math.random()*9+1);
-        check(n);
-        while (check() === false) {
-            nums[n] = Math.floor(Math.random()*9+1);
-            check(n);
+    //generate finished puzzle
+    for (p = 0; p < 82; p++) {
+        t = document.getElementById(p);
+        nums[p] = Math.floor(Math.random()*9+1);        
+        t.innerHTML = nums[p];
+        while (check(nums[p]) === false) {
+            nums[p] = Math.floor(Math.random()*9+1);       
+            t.innerHTML = nums[p];
         }
-    }
-    
-    //populate table
-    for (p = 1; p < 82; p++) {
-        for (r = 0; r < nums.length; r++) {
-            t = document.getElementById(p);
-            if (array.includes(p)) {
-                t.innerHTML = nums[r];
-            } else {
-                td.innerHTML = '<input type="text" />';
-            }
+        //insert text boxes
+        if (array.includes(p) === false) {            
+            td.innerHTML = '<input type="text" />';
         }
     }
     div.appendChild(table);
 }
+
 function check(number) {
-    return true;
+    
+    //declare variables
+    var i;
+    var j;
+    var k;
+    var m;
+    var n;
+    var p;
+    var q;
+    var r;
+    var t;
+    var u;
+    var varj;
+    var varp;
+    var num = number;
+    var clss;
+    var comp1;
+    var comp2;
+    
+    //get first number to compare
+    for (i = 0; i < 82; i++) {
+        comp1 = document.getElementById(i).innerHTML;
+        if (i == 1) {
+        } else {
+            //check row for duplicates
+            for (j = 1; j < 9) {
+                varj = document.getElementById(i);
+                while ((varj % 9) !== 0) {
+                    varj = document.getElementById(i + j);
+                    j++;
+                }
+                k = 9 - j;
+                for (m = 1; m <= k; m++) {
+                    comp2 = document.getElementById(i - m).innerHTML;
+                    if (comp1 == comp2) {
+                        return false;
+                    }
+                }
+                for (n = 1; n < j; n++) {
+                    comp2 = document.getElementById(i + n).innerHTML;
+                    if (comp1 == comp2) {
+                        return false;
+                    }
+                }
+            }
+            //check column for duplicates
+            for (p = 1; p <=9;)
+                varp = document.getElementById(i);
+            while ((varp / 9) < 9) {
+                varp = document.getElementById(i + p);
+                p++;
+            }
+            q = 9 - p;
+            for (r = 1; r <= q; r++) {
+                    comp2 = document.getElementById(i - r).innerHTML;
+                    if (comp1 == comp2) {
+                        return false;
+                    }
+            }
+            for (t = 1; t < p; t++) {
+                comp2 = document.getElementById(i + t).innerHTML;
+                if (comp1 == comp2) {
+                    return false;
+                }
+            }
+            //check box for duplicates
+        }
+    }
 }
