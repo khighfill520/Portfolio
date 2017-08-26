df8eaf9464b4ee6853fe80715948c6b067af2299
Kimber Highfill <khighfill520@gmail.com>
2017-08-26 00:29:45 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -55,7 +55,7 @@ function sudoku(value) {
         }
         //classify cells
         for (m = 1; m < 82; m++) {
-            n = document.getElementById(m);
+            n = parseInt(document.getElementById(m));
             console.log(n);
             if ((m % 3) == 0) {
                 heavy = " heavy_right";
