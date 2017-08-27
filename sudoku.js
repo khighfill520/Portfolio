e191cbe357fe0d6142bbcbf54f1cfc2952eb837c
Kimber Highfill <khighfill520@gmail.com>
2017-08-26 20:24:25 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,40 +1,21 @@
-function sudoku(value) {
+function sudoku() {
     
     //declare variables
-    var diff = value;
-    var div = document.getElementById('puzzle');
+    var div = document.getElementById('buttons');
     var table = document.createElement('table');
-    var easy = [1, 2, 3, 5, 7, 8, 9, 10, 12, 14, 16, 18, 19, 20, 21, 24, 25, 26, 27, 30, 31, 32, 33, 37, 38, 40, 41, 42, 44, 45, 49, 50, 51, 52, 55, 56, 57, 58, 61, 62, 63, 64, 66, 68, 70, 72, 73, 74, 75, 77, 79, 80, 81];
-    var med = [4, 5, 6, 7, 8, 10, 13, 15, 19, 23, 24, 28, 29, 30, 32, 35, 36, 37, 39, 40, 41, 42, 43, 45, 46, 47, 50, 52, 53, 54, 58, 59, 63, 67, 69, 72, 74, 75, 76, 77, 78];
-    var hard = [2, 6, 11, 12, 15, 17, 18, 21, 24, 25, 26, 28, 29, 30, 52, 53, 54, 56, 57, 58, 61, 64, 65, 67, 70, 71, 76, 80];
-    var heavy = "";
     var g;
     var h;
     var nums = [];
     var i;
     var j;
     var k = 1;
-    var m;
-    var n;
-    var p;
-    var q = 0;
-    var r;
-    var t;
     var tr;
     var td;
-    var array;
         
-    //set which pattern to use
-    if (diff = 'Easy') {
-        array = easy;
-    } else if (diff = 'Medium') {
-        array = med;
-    } else {
-        array = hard;
-    }
-    
-    //remove buttons
-    div.innerHTML = "";
+    //change buttons
+    div.innerHTML = '<input type="submit" value="Easy" onClick="play(this.value);" />
+<input type="submit" value="Medium" onClick="play(this.value);" />
+<input type="submit" value="Hard" onClick="play(this.value);" />';
     
     //generate and classify rows
     for (i = 1; i < 10; i++) {
@@ -46,53 +27,78 @@ function sudoku(value) {
         for (j = 1; j < 10; j++) {
             td = tr.insertCell();
             td.id = k;
-            console.log(document.getElementById(k));
             k++;
         }
-        //classify cells
-        for (m = 1; m < 82; m++) {
-            console.log(document.getElementById(m));
-            n = document.getElementById(m);
-            if ((m % 3) == 0) {
-                heavy = " heavy_right";
-            }
-            if (((m / 3) <= 1) && (i <= 3)) {
-                n.className = 'boxA' + heavy;
-            } else if (((m / 3) <= 2) && (i <= 3)) {
-                n.className = 'boxB' + heavy;
-            } else if (((m / 3) <= 3) && (i <= 3)) {
-                n.className = 'boxC' + heavy;
-            } else if (((m / 3) <= 1) && (i <= 6)) {
-                n.className = 'boxD' + heavy;
-            } else if (((m / 3) <= 2) && (i <= 6)) {
-                n.className = 'boxE' + heavy;
-            } else if (((m / 3) <= 3) && (i <= 6)) {
-                n.className = 'boxF' + heavy;
-            } else if (((m / 3) <= 1) && (i <= 9)) {
-                n.className = 'boxG' + heavy;
-            } else if (((m / 3) <= 2) && (i <= 9)) {
-                n.className = 'boxH' + heavy;
-            } else {
-                n.className = 'boxI' + heavy;
-            }
+    }
+    div.appendChild(table);
+}
+
+function play(value) {
+    
+    //declare variables
+    var array;
+    var diff = value;
+    var heavy = "";
+    var tds = document.getElementByTagName('td');
+    var easy = [1, 2, 3, 5, 7, 8, 9, 10, 12, 14, 16, 18, 19, 20, 21, 24, 25, 26, 27, 30, 31, 32, 33, 37, 38, 40, 41, 42, 44, 45, 49, 50, 51, 52, 55, 56, 57, 58, 61, 62, 63, 64, 66, 68, 70, 72, 73, 74, 75, 77, 79, 80, 81];
+    var med = [4, 5, 6, 7, 8, 10, 13, 15, 19, 23, 24, 28, 29, 30, 32, 35, 36, 37, 39, 40, 41, 42, 43, 45, 46, 47, 50, 52, 53, 54, 58, 59, 63, 67, 69, 72, 74, 75, 76, 77, 78];
+    var hard = [2, 6, 11, 12, 15, 17, 18, 21, 24, 25, 26, 28, 29, 30, 52, 53, 54, 56, 57, 58, 61, 64, 65, 67, 70, 71, 76, 80];
+    var i;
+    var j;
+    var k;
+    var m;
+    
+    //set which pattern to use
+    if (diff = 'Easy') {
+        array = easy;
+    } else if (diff = 'Medium') {
+        array = med;
+    } else {
+        array = hard;
+    }    
+    
+    //classify cells
+    for (i = 1; i < 82; i++) {
+        console.log(document.getElementById(i));
+        j = document.getElementById(i);
+        if ((i % 3) == 0) {
+            heavy = " heavy_right";
+        }
+        if (((i / 3) <= 1) && (i <= 3)) {
+            j.className = 'boxA' + heavy;
+        } else if (((i / 3) <= 2) && (i <= 3)) {
+            j.className = 'boxB' + heavy;
+        } else if (((i / 3) <= 3) && (i <= 3)) {
+            j.className = 'boxC' + heavy;
+        } else if (((i / 3) <= 1) && (i <= 6)) {
+            j.className = 'boxD' + heavy;
+        } else if (((i / 3) <= 2) && (i <= 6)) {
+            j.className = 'boxE' + heavy;
+        } else if (((i / 3) <= 3) && (i <= 6)) {
+            j.className = 'boxF' + heavy;
+        } else if (((i / 3) <= 1) && (i <= 9)) {
+            j.className = 'boxG' + heavy;
+        } else if (((i / 3) <= 2) && (i <= 9)) {
+            j.className = 'boxH' + heavy;
+        } else {
+            j.className = 'boxI' + heavy;
         }
     }
     
     //generate finished puzzle
-    for (p = 0; p < 82; p++) {
-        t = document.getElementById(p).innerHTML;
-        nums[p] = Math.floor(Math.random()*9+1);        
-        t = nums[p];
-        while (check(nums[p]) === false) {
-            nums[p] = Math.floor(Math.random()*9+1);       
-            t = nums[p];
+    for (k = 0; k < 82; k++) {
+        m = document.getElementById(k).innerHTML;
+        nums[k] = Math.floor(Math.random()*9+1);        
+        m = nums[k];
+        while (check(nums[k]) === false) {
+            nums[k] = Math.floor(Math.random()*9+1);       
+            m = nums[k];
         }
         //insert text boxes
-        if (array.includes(p) === false) {            
-            td.innerHTML = '<input type="text" />';
+        if (array.includes(k) === false) {            
+            tds.innerHTML = '<input type="text" />';
         }
     }
-    div.appendChild(table);
 }
 
 function check(number) {
