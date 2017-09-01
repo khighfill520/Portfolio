4c906e9315ee3e7bd96c5557d1b411c2e27d3131
Kimber Highfill <khighfill520@gmail.com>
2017-09-01 17:39:18 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -8,7 +8,6 @@ function sudoku() {
     var table = document.createElement('table');
     var g;
     var h;
-    var nums = [];
     var i;
     var j;
     var k;
@@ -56,6 +55,7 @@ function play(value) {
     var easy = [1, 2, 3, 5, 7, 8, 9, 10, 12, 14, 16, 18, 19, 20, 21, 24, 25, 26, 27, 30, 31, 32, 33, 37, 38, 40, 41, 42, 44, 45, 49, 50, 51, 52, 55, 56, 57, 58, 61, 62, 63, 64, 66, 68, 70, 72, 73, 74, 75, 77, 79, 80, 81];
     var med = [4, 5, 6, 7, 8, 10, 13, 15, 19, 23, 24, 28, 29, 30, 32, 35, 36, 37, 39, 40, 41, 42, 43, 45, 46, 47, 50, 52, 53, 54, 58, 59, 63, 67, 69, 72, 74, 75, 76, 77, 78];
     var hard = [2, 6, 11, 12, 15, 17, 18, 21, 24, 25, 26, 28, 29, 30, 52, 53, 54, 56, 57, 58, 61, 64, 65, 67, 70, 71, 76, 80];
+    var nums = [];
     var i;
     var j;
     var k;
@@ -77,19 +77,19 @@ function play(value) {
         k = (i / 9);
         if ((k <= 3) && ((i % 9) <= 3) && ((i % 9) !== 0)) {
             j.className += 'boxA';
-        } else if ((k <= 3) && ((i % 9) <= 6)) {
+        } else if ((k <= 3) && ((i % 9) <= 6) && ((i % 9) !== 0)) {
             j.className += 'boxB';
         } else if ((k <= 3) && (((i % 9) > 6) || ((i % 9) == 0))) {
             j.className += 'boxC';
         } else if ((k <= 6) && ((i % 9) <= 3) && ((i % 9) !== 0)) {
             j.className += 'boxD';
-        } else if ((k <= 6) && ((i % 9) <= 6)) {
+        } else if ((k <= 6) && ((i % 9) <= 6) && ((i % 9) !== 0)) {
             j.className += 'boxE';
         } else if ((k <= 6) && (((i % 9) > 6) || ((i % 9) == 0))) {
             j.className += 'boxF';
         } else if ((k <= 9) && ((i % 9) <= 3) && ((i % 9) !== 0)) {
             j.className += 'boxG';
-        } else if ((k <= 9) && ((i % 9) <= 6)) {
+        } else if ((k <= 9) && ((i % 9) <= 6) && ((i % 9) !== 0)) {
             j.className += 'boxH';
         } else {
             j.className += 'boxI';
