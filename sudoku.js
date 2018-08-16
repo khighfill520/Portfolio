f7ce53698a9f3606efeb88112e80239eb1edd818
Kimber Highfill <khighfill520@gmail.com>
2018-08-16 00:11:08 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -57,7 +57,6 @@ function play(value) {
     var easy = [1, 2, 3, 5, 7, 8, 9, 10, 12, 14, 16, 18, 19, 20, 21, 24, 25, 26, 27, 30, 31, 32, 33, 37, 38, 40, 41, 42, 44, 45, 49, 50, 51, 52, 55, 56, 57, 58, 61, 62, 63, 64, 66, 68, 70, 72, 73, 74, 75, 77, 79, 80, 81];
     var med = [4, 5, 6, 7, 8, 10, 13, 15, 19, 23, 24, 28, 29, 30, 32, 35, 36, 37, 39, 40, 41, 42, 43, 45, 46, 47, 50, 52, 53, 54, 58, 59, 63, 67, 69, 72, 74, 75, 76, 77, 78];
     var hard = [2, 6, 11, 12, 15, 17, 18, 21, 24, 25, 26, 28, 29, 30, 52, 53, 54, 56, 57, 58, 61, 64, 65, 67, 70, 71, 76, 80];
-    var chkplz;
     var i;
     var j;
     var k;
@@ -195,13 +194,10 @@ function check() {
             }
             //check box for duplicates
             cell = document.getElementById(i);
-            console.log(cell.classList);
-            if (!comp1.classList) {
-                continue;
-            } else if (comp1.classList.length == 2) {
-                clss = comp1.classList.item(1);
+            if (cell.classList.length == 2) {
+                clss = cell.classList.item(1);
             } else {
-                clss = comp1.className;
+                clss = cell.className;
             }
             box = document.getElementsByClassName(clss);
             for (u = 0; u < box.length; u++) {
